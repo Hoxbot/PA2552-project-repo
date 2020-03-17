c0c55a535b964f1b0867b7df19ab3c873736ffd2
Hoxbot <hoxbot@unseen.is>
2020-03-17 17:07:00 +0100

    Added SimpleCryption class

---
+++ src/main/java/pa2552_maven_project/SimpleCryption.java
@@ -0,0 +1,31 @@
+package pa2552_maven_project;
+
+import java.lang.String;
+
+public class SimpleCryption {
+	String encrypted_file_name = "encryped.txt";
+	
+	public static void main(String[] args) {
+		//SimpleCryption obj = new SimpleCryption();
+		System.out.println("I HAVE AWOKEN!");
+	}
+	
+	//Functions
+	public String EinsteinLevelEncryption(String msg) {
+		
+		for(int i = 0; i<msg.length(); i++) {
+			int a = (int) msg.charAt(i);
+			
+			if(a >= 64 && a <=122) {
+				a += 1;
+				if (a == 123) {a = 64;}
+			}
+			
+			msg = msg.substring(0, i) + (char)a + msg.substring(i+1);
+			
+		}
+			
+		return msg;
+	}
+	
+}
--- src/main/java/pa2552_maven_project/StartOutClass.java
+++
@@ -1,5 +0,0 @@
-package pa2552_maven_project;
-
-public class StartOutClass {
-	int hi = 0;
-}
