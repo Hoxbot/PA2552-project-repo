6f7bd1147a09e14178b3d7e9a88f4410609d03c9
Hoxbot <hoxbot@unseen.is>
2020-03-21 17:07:44 +0100

    Greenlighting tests

--- src/main/java/pa2552_maven_project/Main.java
+++ src/main/java/pa2552_maven_project/Main.java
@@ -5,8 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		SimpleCryption obj = new SimpleCryption();
 		
-		//String wake_msg = "I HAVE AWOKEN! I OWN 17 DOGS! MY ZEAL IS ENDLESS!\n";
-		String wake_msg = "!![n_|m^u0\n";
+		String wake_msg = "I HAVE AWOKEN! I OWN 17 DOGS! MY ZEAL IS ENDLESS!\n";
 		System.out.println(wake_msg);
 		
 		String crpt_msg = obj.EncryptString(wake_msg);
