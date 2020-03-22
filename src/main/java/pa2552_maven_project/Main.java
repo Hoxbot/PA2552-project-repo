9761512fe70bbcb6a24cefc1d8eb2c8d70b4279a
Hoxbot <hoxbot@unseen.is>
2020-03-22 15:16:06 +0100

    Error in webhook on Git. Re-push.

--- src/main/java/pa2552_maven_project/Main.java
+++ src/main/java/pa2552_maven_project/Main.java
@@ -3,6 +3,7 @@ package pa2552_maven_project;
 public class Main {
 	
 	public static void main(String[] args) {
+		//Create and open window
 		BasicWindow win = new BasicWindow();
 		win.PaintWindow();
 	}
